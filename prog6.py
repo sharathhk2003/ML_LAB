836ef6e9e96151ba310822b701f6355b32cc8bfe
SHARATH <139366667+sharathhk2003@users.noreply.github.com>
2024-07-17 23:15:59 +0530

    Update prog6.py

--- prog6.py
+++ prog6.py
@@ -1,3 +1,96 @@
+#WITHOUT SKLEARN
+import numpy as np
+import pandas as pd
+from collections import Counter
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+def euclidean_distance(x1, x2):
+    return np.sqrt(np.sum((x1 - x2) ** 2))
+
+class KNN:
+    def __init__(self, k, distance_fn):
+        self.k = k
+        self.distance_fn = distance_fn
+    
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+    
+    def predict(self, X):
+        predictions = [self._predict(x) for x in X]
+        return predictions
+    
+    def _predict(self, x):
+        distances = [self.distance_fn(x, x_train) for x_train in self.X_train]
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        most_common = Counter(k_nearest_labels).most_common()
+        return most_common[0][0]
+
+
+df = pd.read_csv("glass.csv")
+print(df.info())
+print(df.describe())
+y = df['Type'].values
+X = df.drop('Type', axis=1).values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+
+plt.figure(figsize=(10, 8))
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm', fmt='.2f', linewidths=.5)
+plt.title('Correlation Matrix Heatmap')
+plt.show()
+
+clf_euclidean = KNN(k=3, distance_fn=euclidean_distance)
+clf_euclidean.fit(X_train, y_train)
+predictions_euclidean = clf_euclidean.predict(X_test)
+accuracy_euclidean = np.sum(predictions_euclidean == y_test) / len(y_test)
+print("Accuracy with Euclidean distance (without sklearn):", accuracy_euclidean)
+
+def manhattan_distance(x1, x2):
+    return np.sum(np.abs(x1 - x2))
+
+
+clf_manhattan = KNN(k=3, distance_fn=manhattan_distance)
+clf_manhattan.fit(X_train, y_train)
+predictions_manhattan = clf_manhattan.predict(X_test)
+accuracy_manhattan = np.sum(predictions_manhattan == y_test) / len(y_test)
+print("Accuracy with Manhattan distance (without sklearn):", accuracy_manhattan)
+
+
+def plot_confusion_matrix(y_true, y_pred, title):
+    cm = confusion_matrix(y_true, y_pred)
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.title(title)
+    plt.show()
+
+plot_confusion_matrix(y_test, predictions_euclidean, "Confusion Matrix for Euclidean Distance")
+plot_confusion_matrix(y_test, predictions_manhattan, "Confusion Matrix for Manhattan Distance")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 #WITH SKLEARN
 import numpy as np
 import pandas as pd
@@ -30,55 +123,4 @@ plt.xlabel('Feature 2')
 plt.ylabel('Feature 3')
 plt.colorbar(label='Type')
 plt.show()
-'''WITHOUT SKLEARN
-import numpy as np
-import pandas as pd
-
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-
-def ec(x1,x2):
-    return np.sqrt(np.sum((x1-x2)**2))
-
-df=pd.read_csv("./glass.csv")
-df.head()
-from collections import Counter
-class KNN:
-    def __init__(self,k=3):
-        self.k=k
-
-    def fit(self,X,y):
-        self.X_train=X
-        self.y_train=y 
-
-    def predict(self,X):
-        predictions=[self._predict(x) for x in X]
-        return predictions
-
-    def _predict(self,x):
-        #Compute distance from one given point to all the points in X_train
-        distances=[ec(x1=x,x2=x_train) for x_train in self.X_train]
-
-        #Get k closest indices and labels
-        k_indices=np.argsort(distances)[:self.k]
-        k_labels=[self.y_train[i] for i in k_indices]
-
-        #Get most common class label
-        co=Counter(k_labels).most_common()
-        return co[0][0]
-
-    
-X=df.drop("Type",axis=1).values
-y=df['Type'].values
-X_train,X_test,Y_train,Y_test=train_test_split(X,y,test_size=0.3,random_state=40)
-clf=KNN(k=3)
-clf.fit(X_train,Y_train)
-predictions=clf.predict(X_test)
-print(predictions)
-plt.scatter(X[:,2],X[:,3],c=y)
-plt.show()
-
-print(accuracy_score(y_pred=predictions,y_true=Y_test))
-
 '''
