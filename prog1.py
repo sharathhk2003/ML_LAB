18f06f892c621efb85a417cd299742a87e4165ac
SHARATH <139366667+sharathhk2003@users.noreply.github.com>
2024-07-17 22:46:28 +0530

    Update prog1.py

--- prog1.py
+++ prog1.py
@@ -1,15 +1,20 @@
 # 1.A
 # 3D Plot
+# 3D Plot
 import pandas as pd
 import matplotlib.pyplot as plt
+
 dataset = pd.read_csv('./ToyotaCorolla.csv')
 x = dataset['KM']
 y = dataset['Doors']
 z = dataset['Price']
+
 ax = plt.axes(projection='3d')
 ax.plot_trisurf(x,y,z,cmap="jet")
 ax.set_title("3D Surface Plot")
+
 plt.show()
+
 
 
 # 1.B
