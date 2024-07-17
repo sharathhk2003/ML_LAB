6891bcd2b43b028049acb1e276006a4721d7d2ea
SHARATH <139366667+sharathhk2003@users.noreply.github.com>
2024-07-17 22:41:47 +0530

    Update prog4a.py

--- prog4a.py
+++ prog4a.py
@@ -1,11 +1,10 @@
+#4.A
 # Box Plot
 import pandas as pd
 import matplotlib.pyplot as plt
-
 data=pd.read_csv('./ToyotaCorolla.csv')
-
 plt.boxplot([data["Price"],data["HP"],data["KM"]])
-
 plt.xticks([1,2,3],["Price","HP","KM"])
-
 plt.show()
+
+#4.B
